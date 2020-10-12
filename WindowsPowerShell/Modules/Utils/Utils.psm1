d72b476dfbaaccc93ea57c3cee10cffed81e304e
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2020-10-12 21:49:35 +0200

    Update Utils.psm1
    Add Github Cli completion

--- WindowsPowerShell/Modules/Utils/Utils.psm1
+++ WindowsPowerShell/Modules/Utils/Utils.psm1
@@ -64,6 +64,13 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
 }
 
 # Github Cli
+
+#Github Cli completion
+#https://github.com/cli/cli/issues/1775#issuecomment-706581004
+gh completion -s powershell | Join-String {
+    $_ -replace " ''\)$"," ' ')" -replace "(^\s+\)\s-join\s';')",'$1 -replace ";$wordToComplete$"' -replace "(\[CompletionResult\]::new\('[\w-]+)'",'$1 '''
+} -Separator "`n" | Invoke-Expression
+
 ${function:pr} = {
     gh pr view --web
 }
